a1488d1c3501fb26e90dec1a94c57e0561b6a5bb
Samira Stivan <elianasamira.stivan@mail.polimi.it>
2020-04-23 21:53:53 +0200

    Added number of repetitions to write a different file every time a scenario is played

--- launch_scenario_runner.py
+++ launch_scenario_runner.py
@@ -3,18 +3,24 @@ import subprocess
 import time
 
 run = True
+num_of_reps = 1
+n = 0
 
-while True:
+while n < num_of_reps:
     time.sleep(10)
     if os.path.isfile("close.txt"):
         run = True
         os.remove("close.txt")
         print("close.txt removed!")
+        n += 1
     if run:
         run = False
-        print("Should launch scenario_runner.py ...")
-        subprocess.Popen(['python', 'scenario_runner.py', '--host',
-                          'dwarf7.local.necst.it', '--scenario', 'VehicleTurningRight_1',
-                          '--reloadWorld'])
-        print("scenario_runner.py launched!")
-    #print(time.time())
+        if n < num_of_reps:
+            print("Should launch scenario_runner.py ...")
+            subprocess.Popen(['python', 'scenario_runner.py', '--host',
+                              'dwarf7.local.necst.it', '--reps', str(n),
+                              '--scenario', 'VehicleTurningRight_1',
+                              '--reloadWorld'])
+
+            print("scenario_runner.py launched! Rep #"+str(n))
+print("All reps done!")
